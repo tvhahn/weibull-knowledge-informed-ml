3295f59fff24accd25805675b49962f1c997fd71
tvhahn <tvhahn@gmail.com>
2022-10-25 08:39:35 -0700

    Fix NASA prognostics download links

--- src/data/download_data_hpc.sh
+++ src/data/download_data_hpc.sh
@@ -6,10 +6,13 @@ mkdir -p bearing_data_raw
 cd bearing_data_raw
 mkdir IMS
 cd IMS
-wget -4 https://ti.arc.nasa.gov/m/project/prognostic-repository/IMS.7z
+wget -4 https://phm-datasets.s3.amazonaws.com/NASA/4.+Bearings.zip
+unzip -o 4.+Bearings.zip
+mv '4. Bearings'/* .
 
 cd ..
 mkdir FEMTO
 cd FEMTO
-wget -4 https://ti.arc.nasa.gov/m/project/prognostic-repository/FEMTOBearingDataSet.zip
-
+wget -4 https://phm-datasets.s3.amazonaws.com/NASA/10.+FEMTO+Bearing.zip
+unzip -o 10.+FEMTO+Bearing.zip
+mv '10. FEMTO Bearing'/* .
